eebadc628121aca8bbef1939330454913b9437d0
Cormac Neeson <neesonch@tcd.ie>
2018-04-30 21:10:38 +0100

    wip: implement rectangle search logic, add and restructure tests

--- rectangle-finder/rectangle-finder.js
+++ rectangle-finder/rectangle-finder.js
@@ -1,6 +1,29 @@
 
-findRectangle = () =>{
-	return true;
+findRectangle = (rows) =>{
+	let rectangle = {};  
+	let rectangleBottom = 0;
+	rows.forEach((line, row) => {
+		let isPartOfTopEdge = false;
+		line.forEach((item, col) => {
+			if(item === 1 && !rectangle.hasOwnProperty('start')){
+				rectangle.start = {row, col};
+				isPartOfTopEdge = true;
+			} else if (item === 1 && rectangleBottom < row){
+				rectangleBottom = row;
+			}
+			if(isPartOfTopEdge){
+				if(item === 0 ){	
+					rectangle.width = col - rectangle.start.col;
+					isPartOfTopEdge = false; 
+				} else if (col === line.length - 1){
+					rectangle.width = col + 1 - rectangle.start.col;
+					isPartOfTopEdge = false; 
+				}	
+			}
+		});
+		rectangle.height = (rectangleBottom + 1 - rectangle.start.row);
+	});
+	return rectangle;
 }
 
 module.exports = findRectangle;
--- rectangle-finder/rectangle-finder.test.js
+++ rectangle-finder/rectangle-finder.test.js
@@ -1,5 +1,23 @@
-rectangleFinder = require(`./rectangle-finder.js`);
+const rectangleFinder = require(`./rectangle-finder.js`);
+const testRectangles = require(`./test-rectangles.js`);
 
-test('test', ()=>{
-	expect(rectangleFinder()).toMatchSnapshot();
+
+test('Rectangle top left', ()=>{
+	expect(rectangleFinder(testRectangles.rectangleTopLeft)).toMatchSnapshot();
+})
+
+test('Rectangle top right', ()=>{
+	expect(rectangleFinder(testRectangles.rectangleTopRight)).toMatchSnapshot();
+})
+
+test('Rectangle bottom left', ()=>{
+	expect(rectangleFinder(testRectangles.rectangleBottomLeft)).toMatchSnapshot();
+})
+
+test('Rectangle bottom right', ()=>{
+	expect(rectangleFinder(testRectangles.rectangleBottomRight)).toMatchSnapshot();
+})
+
+test('Rectangle central', ()=>{
+	expect(rectangleFinder(testRectangles.rectangleCentral)).toMatchSnapshot();
 })
