c9f46d013029aa1661aa50dd5c2d4e8dc821d7f7
Cormac Neeson <neesonch@tcd.ie>
2018-05-02 19:13:42 +0100

    feat: implement evenness-check algorithm/add test cases/add readme

--- is-number-even/is-number-even.js
+++ is-number-even/is-number-even.js
@@ -1,7 +1,9 @@
 isNumberEven = number => {
-  const halfedNumber = number / 2;
-  console.log(halfedNumber);
-  return true;
+	// Remove decimal point to handle floats
+	const defloatedNumber = parseInt(number.toString().replace('.', ''));
+	const halfedNumber = defloatedNumber / 2;
+	// If the half of the given number is equal to the floor of its half, it's even.
+  	return Math.floor(halfedNumber) == halfedNumber;
 };
 
 module.exports = isNumberEven;
--- is-number-even/is-number-even.test.js
+++ is-number-even/is-number-even.test.js
@@ -3,3 +3,15 @@ const isNumberEven = require('./is-number-even.js');
 test('Even integer', () => {
   expect(isNumberEven(22)).toMatchSnapshot();
 });
+
+test('Uneven integer', () => {
+  expect(isNumberEven(11)).toMatchSnapshot();
+});
+
+test('Even float', () => {
+  expect(isNumberEven(10.4)).toMatchSnapshot();
+});
+
+test('Uneven float', () => {
+  expect(isNumberEven(3.5)).toMatchSnapshot();
+});
